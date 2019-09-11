48d4b597231fdae22e1cf209763e529758f25e71
ankaziele <ankaziele@gmail.com>
2019-09-11 14:09:36 +0200

    databinding

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+ newServerName = '';
+ newServerContent = '';
+
+ onAddServer() {
+  // this.serverElements.push({
+  //   type: 'server',
+  //   name: this.newServerName,
+  //   content: this.newServerContent
+  // });
+  // this.newServerName ='';
+  // this.newServerContent ='';
+}
+
+onAddBlueprint() {
+  // this.serverElements.push({
+  //   type: 'blueprint',
+  //   name: this.newServerName,
+  //   content: this.newServerContent
+  // })
+  // this.newServerName ='';
+  // this.newServerContent ='';
+}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
