399576f57d40a9f41cacb88fb5cbe56cdfa11ddb
zieleaz <anna.zielinska@lhsystems.com>
2019-09-30 12:22:34 +0200

    checkbox added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       CockpitComponent,
-      ServerElementComponent
+      ServerElementComponent,
+      CheckboxComponent
    ],
    imports: [
       BrowserModule,
